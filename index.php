c7a4553cd50cfb408e012de95ea716010dd99a4f
JoaquinYllescas <jyllescasruiz@gmail.com>
2016-11-09 16:45:29 +0000

    Setup bien hecho para las plantillas y custom fields

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <?php
 /**
  * The main template file
- *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * E.g., it puts together the home page when no home.php file 
  *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
  *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @package 	WordPress
+ * @subpackage 	Starkers
+ * @since 		Starkers 4.0
  */
-
-get_header(); ?>
-
-	
-<div class="wrapper">
-	<?php
-	get_template_part( "template-intro");
-	/* get_sidebar(); */ 
-	?>
-</div>
+?>
 
 
+<?php if ( have_posts() ): ?>
+<h2>Latest Posts</h2>	
+<ol>
+<?php while ( have_posts() ) : the_post(); ?>
+	<li>
+		<article>
+			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+			<?php the_content(); ?>
+		</article>
+	</li>
+<?php endwhile; ?>
+</ol>
+<?php else: ?>
+<h2>No posts to display</h2>
+<?php endif; ?>
 
-<?php get_footer(); ?>
